9498b2b1e7946405aba13b1b23b68519731d5381
vgalante2 <vgalante0@gmail.com>
2024-04-05 18:52:48 -0400

    created weatherOutput funtion - able to pull city weather data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,8 @@ const baseURL = 'https://api.openweathermap.org/data/2.5'
 const apiKey = 'e9621bce5c4f4f8785b30e2365e66bfb'
 
 
+const weatherSubmit = document.querySelector('#weatherSubmit')
+
 function outputCurrentWeather(currentData) {
 
     const temp = $('#current-temp')
@@ -9,28 +11,91 @@ function outputCurrentWeather(currentData) {
     const humidity = $('#humidity')
 
 
-    temp.text(`Temp: ${currentData.main.temp.toFixed(0)}`)
+    temp.text(`Temp: ${currentData.main.temp.toFixed(0)} °`)
     wind.text(`Wind: ${currentData.wind.speed} mph`)
 
     return currentData.name
 }
 
 
+
+
 function getCurrentWeatherByCity (cityName) {
 
 const url = `${baseURL}/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
-
-
+  
+ 
 console.log("The city is :" + cityName)
-return $.get(url)
+
+return $.get(url).then(function(weatherObj) {
+      
+    localStorage.setItem('currentWeatherData', JSON.stringify(weatherObj))
+     
+       console.log(weatherObj)
+    }
+)
+
+}
+
+
+
+function getForecastWeather(cityName) {
+    const options = `/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
+    const url = baseURL + options
+
+
+    return $.get(url)
+    // return fetch(url)
+    //   .then(function (res) {
+    //     // Pass the parsed json data to the next .then in the chain
+    //     return res.json() // Returns a promise object that must be resolved to get to the data
+    //   })
+  }
+
+
+
+  function storeWeatherData() {
+   
+     const storedData = localStorage.getItem('currentWeatherData')
+
+     if (!storedData) {
+       console.log("no data found")
+
+     }
+
+     const storedWeatherData = JSON.parse(storedData)
+  }
+
+
+
+function outputWeatherData() {
 
 }
 
 
 
 
+weatherSubmit.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    
+    const cityName = document.querySelector('#weatherInput').value
+    
+    if (cityName) {
+        getCurrentWeatherByCity(cityName).then(() => {
+            storeWeatherData()
+        })
+    }else {
+        console.log("please enter a city name")
+    }
+
+})
 
 
 
-getCurrentWeatherByCity('london')
-outputCurrentWeather()
+
+
+
+
+
+
